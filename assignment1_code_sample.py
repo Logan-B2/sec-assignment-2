ee42888d7fc11cb5c39b21f81e0d9ff9a9ae4208
lbrill <lbrill@rrc.ca>
2025-11-05 09:42:25 -0600

    fixed more linting errors

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -4,9 +4,11 @@ Assignment 2 Code Scanning (Assignment 1 Code Example)
 
 import os
 from urllib.request import urlopen
+
 import pymysql
 
 db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
+
 
 def get_user_input():
     """Prompt user for their name and return it."""
@@ -21,7 +23,7 @@ def send_email(to, subject, body):
 
 def get_data():
     """Fetch data from insecure API."""
-    url = 'http://insecure-api.com/get-data'
+    url = "http://insecure-api.com/get-data"
     with urlopen(url) as response:
         data_local = response.read().decode()
     return data_local
@@ -43,44 +45,3 @@ if __name__ == "__main__":
     data_outer = get_data()
     save_to_db(data_outer)
     send_email("admin@example.com", "User Input", user_input_outer)
-
-"""
-
-import os
-import pymysql
-from urllib.request import urlopen
-
-db_config = {
-    'host': 'mydatabase.com',
-    'user': 'admin',
-    'password': 'secret123'
-}
-
-def get_user_input():
-    user_input = input('Enter your name: ')
-    return user_input
-
-def send_email(to, subject, body):
-    os.system(f'echo {body} | mail -s "{subject}" {to}')
-
-def get_data():
-    url = 'http://insecure-api.com/get-data'
-    data = urlopen(url).read().decode()
-    return data
-
-def save_to_db(data):
-    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
-    connection = pymysql.connect(**db_config)
-    cursor = connection.cursor()
-    cursor.execute(query)
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-if __name__ == '__main__':
-    user_input = get_user_input()
-    data = get_data()
-    save_to_db(data)
-    send_email('admin@example.com', 'User Input', user_input)
-
-"""
