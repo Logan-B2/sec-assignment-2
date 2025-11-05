0e973164f84b6471f5955c883a001c67ca0a10fc
lbrill <lbrill@rrc.ca>
2025-11-05 10:17:35 -0600

    fixed some bandit issues

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -3,8 +3,7 @@ Assignment 2 Code Scanning (Assignment 1 Code Example)
 """
 
 import subprocess
-from urllib.request import urlopen
-
+import requests
 import pymysql
 
 db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
@@ -12,13 +11,12 @@ db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
 
 def get_user_input():
     """Prompt user for their name and return it."""
-    user_input_local = input("Enter your name: ")
-    return user_input_local
+    user_input = input("Enter your name: ")
+    return user_input
 
 
 def send_email(to, subject, body):
-    """Send an email using os.system."""
-
+    """Send an email safely using subprocess without shell=True."""
     subprocess.run(
         ["mail", "-s", subject, to],
         input=body.encode("utf-8"),
@@ -27,26 +25,29 @@ def send_email(to, subject, body):
 
 
 def get_data():
-    """Fetch data from insecure API."""
+    """Fetch data from API safely using requests."""
     url = "http://insecure-api.com/get-data"
-    with urlopen(url) as response:
-        data_local = response.read().decode()
-    return data_local
+    # validate URL scheme
+    if not url.startswith(("http://", "https://")):
+        raise ValueError("Invalid URL scheme")
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
 
 
 def save_to_db(data_param):
-    """Save data to the database."""
-    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data_param}', 'Another Value')"
+    """Save data to the database using parameterized query."""
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
-    cursor.execute(query)
+    query = "INSERT INTO mytable (column1, column2) VALUES (%s, %s)"
+    cursor.execute(query, (data_param, "Another Value"))
     connection.commit()
     cursor.close()
     connection.close()
 
 
 if __name__ == "__main__":
-    user_input_outer = get_user_input()
-    data_outer = get_data()
-    save_to_db(data_outer)
-    send_email("admin@example.com", "User Input", user_input_outer)
+    user_input = get_user_input()
+    data = get_data()
+    save_to_db(data)
+    send_email("admin@example.com", "User Input", user_input)
